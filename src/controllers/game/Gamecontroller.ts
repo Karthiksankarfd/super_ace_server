788279dfb6509d0fd14b1c4893dab742d4764a63
Karthiksankarfd <karthikeyansankar887@gmail.com>
2026-05-07 17:20:58 +0530

    fixed bug in checking the valid token and game id is present in the gameController initGameData

--- src/controllers/game/Gamecontroller.ts
+++ src/controllers/game/Gamecontroller.ts
@@ -83,7 +83,7 @@ class Gamecontroller {
       // all should happen in a linear way if one thing fails we might face issue for sure 
       await updateBalanceFromAccount({ ...req.body, winAmount: spinResult.spinSessionTotal }, "CREDIT")
       await mysqlGameRepoServices.insertSpinData(spinIdCacheKey, "dev-team0o090", betAmount, "priti--2222", spinResult.spinSessionTotal, "transaction-123", JSON.stringify(req.body) , next)
-      await deleteCache(`${user_id}-spinId`);
+      await deleteCache(`${user_id}_spinId`);
       console.log("Im running")
       console.log(chalk.bgMagenta("|--------------------------THE SPIN CYCLE HAS ENDED---------------------------|"));
       return res.status(200).json({ spinId: spinIdCacheKey, status:"SUCCESS", msg:`successfully executed spin with spinId ${spinIdCacheKey}`, spinSessionTotal:spinResult.spinSessionTotal,  initialGridState, spinResult, scatterPhaseResultData });
